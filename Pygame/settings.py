193b6f41940ad061e31c0589ce5828618c8403ff
maysa-bik <maysa.bik@laplateforme.com>
2024-03-21 00:32:01 +0100

    créer un fichier pygame et la 1ere essayer avec le pygame

---
+++ Pygame/settings.py
@@ -0,0 +1,22 @@
+# Colors 
+White = (255, 255, 255)
+Black = (0, 0, 0)
+DarkGrey = (40, 40, 40)
+LightGrey = (100, 100, 100)
+Green = (0, 255, 0)
+DarkGreen = (0, 200, 0)
+Blue = (0, 0, 255)
+Red = (255, 0, 0)
+Yellow = (255, 255, 0)
+BGCOLOUR = DarkGrey
+
+# game paramaitres :
+TileSize = 32
+Rows = 15
+Cols = 15
+Amount_Mines = 5
+WIDTH = TileSize * Rows
+HEIGHT = TileSize * Cols
+FPS = 60
+Title = "MinesWeeper"
+

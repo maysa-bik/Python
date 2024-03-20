0104aed18721fcc9943673e9d5ef5db88eef3ecc
maysa-bik <maysa.bik@laplateforme.com>
2024-03-20 13:01:14 +0100

    commencer mettre de button avec le fonction

--- Tkinter/unit.py
+++ Tkinter/unit.py
@@ -9,8 +9,19 @@ class Cell:
     all = []
     def __init__(self, is_mine=False):
         self.is_mine = is_mine
+        self.cell_btn_object = None
 
     def create_btn_object(self, location):
         btn = Button(
             location,
+            width=12,
+            height= 4,
         )   
+        btn.bind('<Button-1>', self.left_click_actions)
+        btn.bind('<Button-3>', self.right_click_actions)
+
+    def show_mine(self):
+        self.cell_btn_object.configure(bg="red")
+        ctypes.windll.user32.messageBoxW(0,"vous clickez sur a mine", "rejouer", 0)
+        sys.exit()
+        

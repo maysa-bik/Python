357e27ec2492682ae13f1d4bbfc1190eef70067d
maysa-bik <maysa.bik@laplateforme.com>
2024-03-21 09:22:17 +0100

    créer 2 fichier un pour la parametre et 2 pour le fonction de petite carée

--- Pygame/settings.py
+++ Pygame/settings.py
@@ -1,4 +1,8 @@
 # Colors 
+import os
+import pygame
+
+
 White = (255, 255, 255)
 Black = (0, 0, 0)
 DarkGrey = (40, 40, 40)
@@ -20,3 +24,14 @@ HEIGHT = TileSize * Cols
 FPS = 60
 Title = "MinesWeeper"
 
+tile_numbers = []
+for i in range(1, 9):
+    #  pour changer le size de photo (pygame.transform.scale) avec le size de carée pour ça on mis (TileSize, TileSize)
+    tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("Photoes", f"Tile{i}.png")), (TileSize, TileSize)))
+
+tile_empty = (pygame.image.load(os.path.join("Photoes", f"TileEmpty.png")), (TileSize, TileSize))    
+tile_exploded = (pygame.image.load(os.path.join("Photoes", f"TileExploded.png")), (TileSize, TileSize)) 
+tile_flag = (pygame.image.load(os.path.join("Photoes", f"TileFlag.png")), (TileSize, TileSize)) 
+tile_mine = (pygame.image.load(os.path.join("Photoes", f"TileMine.png")), (TileSize, TileSize)) 
+tile_unknown = (pygame.image.load(os.path.join("Photoes", f"TileUnknown.png")), (TileSize, TileSize)) 
+tile_not_mine = (pygame.image.load(os.path.join("Photoes", f"TileNotMine.png")), (TileSize, TileSize)) 
--- Pygame/sprites.py
+++ Pygame/sprites.py
@@ -1,2 +1,12 @@
 import pygame
 from settings import *
+
+# types list
+# "." -> unknown (غير معروف)
+# "x" -> mine ( boombe / لغم)
+# "c" -> clue ( index)
+# "/" -> empty (vide /  فارغ)
+
+class Tile:
+    def __init__(self, x, y, image, type):
+        pass

13aaba4959df7dc18886ec2a34cf53b6e308000a
maysa-bik <maysa.bik@laplateforme.com>
2024-03-22 12:41:47 +0100

    je fais un methode pour afficher le 8 petit carée acouté de Mine

--- Pygame/sprites.py
+++ Pygame/sprites.py
@@ -4,8 +4,8 @@ from settings import *
 
 # types list
 # "." -> unknown (غير معروف)
-# "x" -> mine ( boombe / لغم)
-# "c" -> clue ( index)
+# "X" -> mine ( boombe / لغم)
+# "C" -> clue ( index)
 # "/" -> empty (vide /  فارغ)
 
 class Tile:
@@ -43,6 +43,7 @@ class Board:
        """
        self.board_list = [[Tile(col, row, tile_empty, ".") for row in range(ROWS)] for col in range(COLS)] 
        self.place_mines()
+       self.place_clues()
 
     def place_mines(self):
         for _ in range(AMOUNT_MINES):
@@ -54,6 +55,34 @@ class Board:
                     self.board_list[x][y].image = tile_mine
                     self.board_list[x][y].type = "X"
                     break
+    
+    def place_clues(self):
+        for x in range(ROWS):
+            for y in range(COLS):
+                if self.board_list[x][y].type != "X":
+                    total_mines = self.check_neighbours(x, y)
+                    if total_mines > 0:
+                        self.board_list[x][y].image = tile_numbers[total_mines-1]
+                        self.board_list[x][y].type = "C"
+
+
+    @staticmethod # طريقة ثابتة 
+    def is_inside(x, y):
+        return 0 <= x < ROWS and 0 <= y < COLS # اذا كان اقل او يساوي عدد الصفوف / الاعمدة 
+
+    #نستدعي هذه الطريقة لنعرف المربع المجاور الذب نتحقق منه موجودا بالفعل داخل اللوحات لانه اذا لم يكم الاكر كذلك فلا يمكننا 
+    def check_neighbours(self, x, y):
+        total_mines = 0
+        for x_offset in range(-1, 2):
+            for y_offset in range(-1, 2):
+                neighbour_x = x + x_offset
+                neighbour_y = y + y_offset
+                # اريد ان اتاكد من ان الجار x والجار y  موجودان داخل اللوحات واذا لم يكن كذلك لم يكن لديك هذا التحقق فسيتم تكراره وستعطيه خطأ  نريج التحقق ممل اذا كانت هذه الاحذاثيات بالداخل 
+                # نحن منحقق ايضا مما اذا كان هذا الجار منجما 
+                if self.is_inside(neighbour_x, neighbour_y) and self.board_list[neighbour_x][neighbour_y].type == "X":
+                    total_mines += 1
+
+        return total_mines        
 
 
     
